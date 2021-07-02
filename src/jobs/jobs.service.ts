ec8cb4f0f81f0b94a28893d8cbd18d65fbc92ea6
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2021-07-02 16:56:51 +0500

    add job.servies.ts

---
+++ src/jobs/jobs.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Job } from './interfaces/jobs.interface';
+
+@Injectable()
+export class JobsService {
+  constructor(@InjectModel('Job') private readonly jobModel: Model<Job>) {}
+
+  async getJobs(): Promise<Job[]> {
+    return await this.jobModel.find().sort('title');
+  }
+
+  async findJob(id: string): Promise<Job> {
+    return await this.jobModel.findById(id).exec();
+  }
+
+  async createJob(job: Job): Promise<Job> {
+    const product = new this.jobModel(job);
+    return await product.save();
+  }
+
+  async updateJob(id: string, { title, salary }: Job): Promise<Job> {
+    const product = await this.jobModel
+      .findByIdAndUpdate(id, { title, salary }, { new: true })
+      .exec();
+
+    if (!product) throw new NotFoundException('Job not found.');
+
+    return product;
+  }
+
+  async deleteJob(id: string): Promise<Job> {
+    return this.jobModel.findByIdAndRemove(id);
+  }
+}
